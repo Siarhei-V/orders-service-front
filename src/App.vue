72bd5b74ec06920b506c7655451c087e48777e7d
Serge V <serge.vasileuski@gmail.com>
2023-11-30 19:59:32 +0300

    feat: implement order view and order deletion

--- src/App.vue
+++ src/App.vue
@@ -79,10 +79,57 @@
       :items-per-page="5"
       class="elevation-1 row-pointer"
       @click:row="handleClick"
+      :loading="isLoadingActive"
+      loading-text="Загружаю. Подождите"
+      no-data-text="Нет данных"
     >
     </v-data-table>
 
-
+    <template>
+      <div class="text-center">
+        <v-dialog
+          v-model="isDialogOpened"
+          width="600"
+        >
+          <v-card>
+            <v-card-title class="text-h5 grey lighten-2">
+              Заказ № '{{selectedRow.number}}' от {{selectedOrderDate}}, поставщик: '{{selectedProviderName}}'
+            </v-card-title>
+            <template>
+              <v-simple-table
+                fixed-header
+                height="300px"
+              >
+                <template v-slot:default>
+                  <thead>
+                    <tr>
+                      <th class="text-center">Имя</th>
+                      <th class="text-center">Количество</th>
+                      <th class="text-center">Единица</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr v-for="item in orderItems" :key="item.id">
+                      <td>{{ item.name }}</td>
+                      <td>{{ item.quantity }}</td>
+                      <td>{{ item.unit }}</td>
+                    </tr>
+                  </tbody>
+                </template>
+              </v-simple-table>
+               <v-row class="ma-0">
+                  <v-col cols="6" class="d-flex justify-center">
+                    <v-btn small color="primary" elevation="5">Редактировать</v-btn>
+                  </v-col>
+                  <v-col cols="6" class="d-flex justify-center">
+                    <v-btn small color="error" elevation="5" @click="removeOrder(selectedRow.id)">Удалить</v-btn>
+                  </v-col>
+                </v-row>
+            </template>
+          </v-card>
+        </v-dialog>
+      </div>
+    </template>
   </v-app>
 </template>
 
@@ -105,13 +152,36 @@ export default {
     dateTo: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().slice(0, 10),
     isDateFromOpened: false,
     isDateToOpened: false,
+    selectedRow: '',
+    selectedProviderName: '',
+    selectedOrderDate: '',
+    orderItems: [],
+    isLoadingActive: false,
   }),
   methods: {
     handleClick: function(row) {
+      // TODO: show popup when data was received only
+      axios.get(this.baseUrl + '/api/v1/order-items', {
+        params: {
+          orderId: row.id,
+        }
+      })
+      .then(response => {
+        if (response.status === 200) {
+          this.orderItems = response.data.data;
+        }
+      })
+      .catch(error => {
+        this.orderItems = [];
+        console.log(error);
+      });
       this.isDialogOpened = true;
-      console.log(row);
+      this.selectedRow = row;
+      this.selectedProviderName = row.provider.name;
+      this.selectedOrderDate = row.date.slice(0, 10);
     },
     getOrders: function() {
+      this.isLoadingActive = true;
       axios.get(this.baseUrl + '/api/v1/orders', {
         params: {
           dateFrom: this.dateFrom,
@@ -121,9 +191,24 @@ export default {
       .then(response => {
         if (response.status === 200) {
           this.orders = response.data.data;
+          this.isLoadingActive = false;
         }
       })
-      .catch(function(error) {
+      .catch(error => {
+        console.log(error);
+        this.isLoadingActive = false;
+      });
+    },
+    removeOrder: function(id) {
+      axios.delete(this.baseUrl + `/api/v1/orders/order/${id}`)
+      .then(response => {
+        if (response.status === 200) {
+          // TODO: show message
+          this.isDialogOpened = false;
+          this.getOrders();
+        }
+      })
+      .catch(error => {
         console.log(error);
       });
     }
